d82f78da1799ea4742ace7241c76a8d52dc57313
Paul Ulerick <p.ulerick17@gmail.com>
2022-10-19 19:14:25 -0400

    made nav bar for landing page, fixed bug where styles weren't displaying in browser

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const res = require("express/lib/response");
 const { send, sendStatus } = require("express/lib/response");
+const { dirname } = require("path");
+const path = require("path");   
 
 // Makes the express app
 const app = express();
@@ -11,6 +13,10 @@ app.listen('3000', () => {
 
 // Allows the html page and css styles to be rendered
 app.set("view engine", "ejs");
+
+// Makes the css and html files accessable
+app.use(express.static(path.join(__dirname, "/views")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Recieves get request and renders landing page
 app.get('/', (req,res) => {
