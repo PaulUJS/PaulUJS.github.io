2d85e3214bd8e72fff39db1e3d0600ab490962e3
Paul Ulerick <p.ulerick17@gmail.com>
2022-10-20 19:09:04 -0400

    Edited contact form for sending me emails, made the email functionality and linked it to the contact page

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const res = require("express/lib/response");
-const { send, sendStatus } = require("express/lib/response");
+const { send, sendStatus, json } = require("express/lib/response");
 const { dirname } = require("path");
-const path = require("path");   
+const path = require("path");
+const nodemailer = require("nodemailer");
 
 // Makes the express app
 const app = express();
 
-app.listen('3000', () => {
-    console.log('server running on local host 3000')
-})
+app.listen(process.env.PORT, () => {})
+
+// Gets form info from html forms and reads as json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Allows the html page and css styles to be rendered
 app.set("view engine", "ejs");
@@ -37,3 +40,33 @@ app.get('/projects', (req, res) => {
 app.get('/contactme', (req, res) => {
     res.render('contact')
 })
+
+app.post('/contactme', (req, res) => {
+    let email = JSON.stringify(req.body.email)
+    let name = JSON.stringify(req.body.name)
+    let subject = JSON.stringify(req.body.subject)
+    let content = JSON.stringify(req.body.content)
+    
+    mail(email, subject, content)
+})
+
+// Function that grabs the html submitted in the email form and sends an email with that content to me
+async function mail(email, subject, content) {
+    // creates the email object that will be sending reminder emails
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_SERVICE,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    // send mail with defined transport object
+    let email_details = await transporter.sendMail({
+        from: email, // sender address
+        to: process.env.EMAIL_SERVICE, // My email
+        subject: subject, // Subject line
+        text: content // plain text body
+    });
+
+}
