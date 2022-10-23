1f7a8e3633f5bb42d069910167748967deadb730
Paul Ulerick <p.ulerick17@gmail.com>
2022-10-23 19:57:11 -0400

    Got rid of routes since only serving static page, worked on making email functionality

--- index.js
+++ index.js
@@ -1,65 +1,15 @@
-const express = require("express");
-const res = require("express/lib/response");
-const { send, sendStatus, json } = require("express/lib/response");
-const { dirname } = require("path");
-const path = require("path");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const EMAIL_SERVICE = process.env.EMAIL_SERVICE
 const EMAIL_PASS = process.env.EMAIL_PASS
 
-// Makes the express app
-const app = express();
-
-app.listen("3000", () => {})
-
-// Gets form info from html forms and reads as json
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Allows the html page and css styles to be rendered
-//app.set("view engine", "ejs");
-
-// Makes the css and html files accessable
-//app.use(express.static(path.join(__dirname, "/views")));
-//app.use(express.static(path.join(__dirname, "/public")));
-
-// Recieves get request and renders landing page
-app.get('/', (req,res) => {
-    res.sendFile( "/index.html")
-})
-
-// Renders about me page
-app.get('/aboutme', (req, res) => {
-    res.sendFile( "/aboutme.html")
-})
-
-// Renders projects page
-app.get('/projects', (req, res) => {
-    res.sendFile("/projects.html")
-})
-
-// Renders contact page
-app.get('/contactme', (req, res) => {
-    res.sendFile("/contact.html")
-})
-
-// Runs once contactme form is submitted and sends an email to me with the forms content
-app.post('/contactme', (req, res) => {
-    // Grabs the html form data and parses it
-    let email = req.body.email
-    let subject = JSON.stringify(req.body.subject)
-    let message = JSON.stringify(req.body.message)
-    
-    // Calls function that sends the email
-    mail(email, subject, message);
-    
-    res.redirect('/')
-})
-
 // Function that grabs the html submitted in the email form and sends an email with that content to me
-async function mail(email, subject, message) {
+async function mail() {
+    let email = document.getElementsByName("email")
+    let subject = document.getElementsByName("subject")
+    let message = document.getElementsByName("message")
+
     try {
         const transport = nodemailer.createTransport({
             service: 'gmail',
